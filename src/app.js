3bde3cb52d0d1476b149e06badb92b331bc82112
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-12-01 18:02:16 -0800

    Imports bluprint js.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
+import SettingsProvider from './context/settings'
 import ToDo from './components/todo/todo.js';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <ToDo />
+      <SettingsProvider>
+        <ToDo />
+      </SettingsProvider>
     );
   }
 }
