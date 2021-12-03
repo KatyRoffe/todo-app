1ce595352c598ebe89064cf6b5a407f134e5f305
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-12-02 16:20:57 -0800

    Updates to include hide feature on UI.

--- src/context/settings.js
+++ src/context/settings.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 
 export const SettingsContext = React.createContext();
 
-function Settings(props) {
+function SettingsProvider(props) {
 
   const [hide, setHide] = useState(false);
   const [displayNumberOfItems, setDisplayNumberOfItems] = useState(5);
   const [sort, setSort] = useState('')
 
   return (
-    <SettingsContext.Provider value={{hide, displayNumberOfItems, sort}}>
+    <SettingsContext.Provider value={{hide, displayNumberOfItems, sort, setHide}}>
       {props.children}
     </SettingsContext.Provider>
   )
 }
 
-export default Settings;
+export default SettingsProvider;
