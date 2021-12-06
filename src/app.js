6482c40ce5d83591c6c9b403d5009f605c484366
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-12-05 23:08:36 -0800

    imports auth/login.

--- src/app.js
+++ src/app.js
@@ -4,14 +4,21 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 import SettingsProvider from './context/settings'
 import ToDo from './components/todo/todo.js';
+import Login from './components/auth/login.js';
+import Auth from './components/auth/auth.js'
 
 
 export default class App extends React.Component {
   render() {
     return (
-      <SettingsProvider>
-        <ToDo />
-      </SettingsProvider>
+    <>
+      <Login />
+      <Auth capability="read">
+        <SettingsProvider>
+          <ToDo />
+        </SettingsProvider>
+      </Auth>
+    </>
     );
   }
 }
